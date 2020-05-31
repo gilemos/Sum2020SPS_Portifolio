ef34bd6b7037492dd11b758c53c1ec8b6372619b
Giovanna Lemos <gigi18377@gmail.com>
2020-05-31 13:23:30 -0400

    Makes project menu

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,18 +1,3 @@
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-    const greetings =
-      ['We are all stories in the end. Just make it a good one, eh? (Doctor Who)', 'Your assumptions are your windows on the world. Scrub them off every once in a while, or the light will not come in. (Isaac Asimov)', 'If you wish to make an apple pie from scratch, you must first invent the universe. (Carl Sagan)', 'Time is an illusion. Lunchtime doubly so. (The Hitchhikers Guide to the Galaxy)'];
-
-    // Pick a random greeting.
-    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-    // Add it to the page.
-    const greetingContainer = document.getElementById('greeting-container');
-    greetingContainer.innerText = greeting;
-}
-
 
 window.onscroll = function() {scrollFunction()};
 
@@ -33,43 +18,54 @@ function scrollFunction() {
 
 
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-        document.getElementById("miniBioText").style.opacity = 1;
-        document.getElementById("bioWavyLine").style.opacity = 1;
-        document.getElementById("miniBioHeader").style.opacity = 1;
+        document.getElementById("miniBioDiv").style.opacity = 1;
         document.getElementById("gifPositionerDiv").style.opacity = 1;
         document.getElementById("mainHello").style.display= "none";
         document.getElementById("socialMediaLinksDiv").style.display = "none";
     } else {
-        document.getElementById("miniBioText").style.opacity = 0;
-        document.getElementById("bioWavyLine").style.opacity = 0;
-        document.getElementById("miniBioHeader").style.opacity = 0;
+        document.getElementById("miniBioDiv").style.opacity = 0;
         document.getElementById("gifPositionerDiv").style.opacity = 0;
     }
 
     if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
-        document.getElementById("miniBioText").style.opacity = 0;
-        document.getElementById("bioWavyLine").style.opacity = 0;
-        document.getElementById("miniBioHeader").style.opacity = 0;
+        document.getElementById("miniBioDiv").style.opacity = 0;
         document.getElementById("gifPositionerDiv").style.opacity = 0;
     } else {
-        document.getElementById("miniBioText").style.display= "block";
-        document.getElementById("miniBioHeader").style.display= "block";
+        document.getElementById("miniBioDiv").style.display= "block";
         document.getElementById("gifPositionerDiv").style.display = "block";
     }
 
     if ( document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
-        document.getElementById("parallaxBlack").style.height = "0px";
-        document.getElementById("miniBioText").style.display= "none";
-        document.getElementById("miniBioHeader").style.display= "none";
+        document.getElementById("parallaxDiv").style.height = "0px";
+        document.getElementById("miniBioDiv").style.display= "none";
         document.getElementById("gifPositionerDiv").style.display = "none";
         document.getElementById("projectHeader").style.opacity = 1;
         document.getElementById("projectDescription").style.opacity = 1;
         document.getElementById("galleryContainer").style.opacity = 1;
     } else {
-        document.getElementById("parallaxBlack").style.height = "100vh";
+        document.getElementById("parallaxDiv").style.height = "100vh";
         document.getElementById("projectHeader").style.opacity = 0;
         document.getElementById("projectDescription").style.opacity = 0;
         document.getElementById("galleryContainer").style.opacity = 0;
     }
 
+}
+
+function eraseAllFigures() {
+    var x = document.querySelectorAll(".galleryItem");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.opacity = "0";
+        x[i].style.display = "none";
+    }
+}
+
+function selectFigure(className) {
+    eraseAllFigures();
+    var x = document.querySelectorAll(className);
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "block";
+        x[i].style.opacity = "1";
+    }
 }
